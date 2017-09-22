fda92a715328a749867ae5938bee81c66a7bc4ea
pankajpansari <pankaj.pansari1@gmail.com>
2017-09-22 20:05:35 +0100

    Accept command line arguement - unary filename, and potts weight

--- examples/inference_sparse.cpp
+++ examples/inference_sparse.cpp
@@ -1,27 +1,37 @@
 #include <chrono>
+#include <iostream>
 #include <fstream>
 #include <string>
 #include <cstddef>
 #include <vector>
+#include <assert.h>
 #include "densecrf.h"
 #include "sparsecrf.h"
 #include "file_storage.hpp"
 #undef NDEBUG
 #include <assert.h>
 
-int main(){
+//const std::string FOLDER_NAME = "/home/pankaj/Truncated_Max_of_Convex/data/input/synthetic/unaries/"; 
+
+int main(int argc, char *argv[]){
     int H = 100; 
     int M = 20;
 
+    assert(argc == 3 && "usage: ./inference_sparse unary_0.txt 1");
     SparseCRF crf(H, H, M);
     MatrixXf unary = MatrixXf::Zero(M, H*H);
-    load_unary_synthetic("/home/pankaj/Truncated_Max_of_Convex/data/input/synthetic/unaries/unary_0.txt", H*H, M, unary);
+
+    std::string arg1(argv[1]);
+
+    std::string unary_file = "/home/pankaj/Truncated_Max_of_Convex/data/input/synthetic/unaries/" + arg1;
+
+    load_unary_synthetic(unary_file, H*H, M, unary);
  
     crf.setUnary(unary);
-    crf.setPottsWeight(1);
     
-//    std::cout << crf.getUnary() << std::endl; 
-//    MatrixXf unary = crf.readUnary("/home/pankaj/SubmodularInference/data/working/11_09_2017/unary.txt", M, H);
-//    int grid_size = H; 
+    float weight = std::stof(argv[2]);
+    std::cout << weight << std::endl;
+    crf.setPottsWeight(weight);
+    
     crf.submodularFrankWolfe(unary, H);
 }
